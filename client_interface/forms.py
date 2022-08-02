2fc1a42d6efd502c7c9c547f241e7fd1d0d2b4de
spacecat <blablabla@gmail.com>
2022-08-02 10:02:01 +0300

    creation forms templates

--- client_interface/forms.py
+++ client_interface/forms.py
@@ -3,13 +3,13 @@ from courses.models import Course, Action, Step
 
 
 class CourseCreationForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    description = forms.CharField(max_length=255)
+    title = forms.CharField(max_length=100, widget=forms.TextInput)
+    description = forms.CharField(max_length=255, widget=forms.Textarea)
     cover = forms.ImageField(widget=forms.FileInput, required=False)
     
 
 class ActionCreationForm(forms.Form):
-    title = forms.CharField(max_length=100)
+    title = forms.CharField(max_length=100, widget=forms.TextInput)
     cover = forms.ImageField(widget=forms.FileInput, required=False)
 
 class StepCreationForm(forms.Form):
